7f369e2d6258e177cfefa56610fd8d06db2801ac
Spasoje <117464773+spasoje2001@users.noreply.github.com>
2025-12-10 00:18:05 +0100

    Feature super admin setup (#22)
    * feat: implement super admin setup command (#1)
    
    * create setup_admin command that generate password and writes it to admin_password.txt
    * unit tests
    
    * chore: update app routing
    * create new core project module which is used for the home page
    * setup routing in urls.py to include urls.py configs from all other modules
    * setup an initial home.html page

---
+++ app/analytics/urls.py
@@ -0,0 +1,8 @@
+"""URL configuration for analytics app."""
+from django.urls import path
+
+app_name = 'analytics'
+
+urlpatterns = [
+    # path('', views.dashboard, name='dashboard'),
+]
---
+++ app/repositories/urls.py
@@ -0,0 +1,9 @@
+"""URL configuration for repositories app."""
+from django.urls import path
+
+app_name = 'repositories'
+
+urlpatterns = [
+    # path('', views.user_repositories, name='user_repositories'),
+    # path('<str:owner>/<str:name>/', views.repository_detail, name='detail'),
+]
