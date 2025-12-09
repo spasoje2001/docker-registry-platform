9d52f123057e4c991b48b06b73f82fdbc1d65fa1
lukamilanko <milankoluka01@gmail.com>
2025-12-09 15:59:49 +0100

    comments deleted

--- app/repositories/admin.py
+++ app/repositories/admin.py
@@ -4,19 +4,14 @@ from .models import Repository
 
 @admin.register(Repository)
 class RepositoryAdmin(admin.ModelAdmin):
-    # Koje kolone se prikazuju u listi
     list_display = ("full_name", "owner", "visibility", "is_official", "created_at")
 
-    # Filteri sa desne strane
     list_filter = ("visibility", "is_official", "created_at")
 
-    # Pretraga
     search_fields = ("name", "owner__username", "description")
 
-    # Polja koja ne mogu da se menjaju
     readonly_fields = ("created_at", "updated_at")
 
-    # Organizacija polja u grupe
     fieldsets = (
         ("Basic Information", {"fields": ("name", "description", "owner")}),
         ("Settings", {"fields": ("visibility", "is_official")}),
